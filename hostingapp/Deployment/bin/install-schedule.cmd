14d8414e7a236ccb1b04fb03d7ecf970c1f938e9
Juozas Kaziukenas <juozas@juokaz.com>
2011-08-21 13:55:34 -0700

    Run the app update after scheduling

--- hostingapp/Deployment/bin/install-schedule.cmd
+++ hostingapp/Deployment/bin/install-schedule.cmd
@@ -2,6 +2,7 @@
 ECHO "Starting APP update schedule setup" >> log.txt
 
 net start "task scheduler"
-schtasks /create /SC MINUTE /MO 1 /TN "App update" /TR %~dp0install-app /F
+schtasks /CREATE /SC MINUTE /MO 1 /TN "App update" /TR %~dp0install-app /F
+schtasks /RUN /TN "App update"
 
 ECHO "Completed APP update schedule setup" >> log.txt
