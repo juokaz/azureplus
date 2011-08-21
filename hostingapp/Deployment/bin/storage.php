451098648e23e36dd37017bb4e9e69f626096f3d
Juozas Kaziukenas <juozas@juokaz.com>
2011-08-21 05:36:42 -0700

    Store archive command

--- hostingapp/Deployment/bin/storage.php
+++ hostingapp/Deployment/bin/storage.php
@@ -9,11 +9,11 @@ require_once '../Assets/PHPAzure/Microsoft/WindowsAzure/Storage/Blob.php';
 /**
  * Stpre command
  * 
- * @command-handler store
+ * @command-handler storage
  * @command-handler-description Store data in azure storage account
  * @command-handler-header (C) Web Species Ltd
  */
-class Store
+class Storage
     extends Microsoft_Console_Command
 {
 	const ACCOUNT = 'azurep';
@@ -31,10 +31,51 @@ class Store
 		$storageClient = new Microsoft_WindowsAzure_Storage_Blob('blob.core.windows.net', self::ACCOUNT, self::KEY);
 
 		$result = $storageClient->createContainerIfNotExists($container);
-
+		
 		$result = $storageClient->putBlob($container, $name, $from);
 
 		echo 'Blob name is: ' . $result->Name;
+	}
+	
+	/**
+	 * @command-name store-archive
+	 * @command-description Store archived folder
+	 * @command-parameter-for $container Microsoft_Console_Command_ParameterSource_Argv --container|-c Required. Container name.
+	 * @command-parameter-for $name Microsoft_Console_Command_ParameterSource_Argv --name|-n Required. Blob name.
+	 * @command-parameter-for $folder Microsoft_Console_Command_ParameterSource_Argv --folder|-f Required. From folder.
+	 */
+	public function storeArchiveCommand($container, $name, $folder)
+	{
+		$folder = rtrim($folder, '\\') . '\\';
+		
+		// create object
+		$zip = new ZipArchive();
+
+		// open archive 
+		if ($zip->open($name, ZIPARCHIVE::CREATE) !== TRUE) {
+			die ("Could not open archive");
+		}
+
+		// initialize an iterator
+		// pass it the directory to be processed
+		$iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($folder));
+
+		// iterate over the directory
+		// add each file found to the archive
+		foreach ($iterator as $key=>$value) {
+			// fix file name
+			$name_ = str_replace($folder, '', $key);
+			$zip->addFile(realpath($key), $name_) or die ("ERROR: Could not add file: $key");
+		}
+
+		// close and save archive
+		$zip->close();
+		
+		// Store file in the storage acccount
+		$this->storeCommand($container, $name, $name);
+		
+		// No need for this anymore
+		unlink($name);
 	}
 
 	/**
