bec5aa31c4d471b97de623b2d20d75e016013120
Juozas Kaziukenas <juozas@juokaz.com>
2011-08-21 09:08:22 -0700

    Update sitesroot folder if new files exist

--- hostingapp/Deployment/bin/install-app.cmd
+++ hostingapp/Deployment/bin/install-app.cmd
@@ -1,7 +1,11 @@
 @echo off
 ECHO "Starting APP installation from storage account" >> log.txt
 
-rd /s /q "..\..\sitesroot\0\"
-php.exe storage.php update -c=apps -e=app.zip -n=sample.zip -t="..\..\sitesroot\0\" >> log.txt 2>>err.txt
+php.exe storage.php update -c=apps -e=app.zip -n=sample.zip -t="..\..\sitesroot\0_new" >> log.txt 2>>err.txt
+
+if exist "..\..\sitesroot\0_new" (
+	rd /s /q "..\..\sitesroot\0\"
+	ren "..\..\sitesroot\0_new" "0"
+)
 
 ECHO "Completed APP Installation" >> log.txt
