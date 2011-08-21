cd96178b1ddf13f4d6c43b70ee81c33a4dda8b1d
Juozas Kaziukenas <juozas@juokaz.com>
2011-08-21 15:11:01 -0700

    Run APP update rather as system task

--- hostingapp/Deployment/bin/install-schedule.cmd
+++ hostingapp/Deployment/bin/install-schedule.cmd
@@ -2,10 +2,9 @@
 ECHO "Starting APP update schedule setup" >> log.txt
 
 net start "task scheduler"
-net user scheduler SecretP@ssw0rd /add
-net localgroup Administrators scheduler /add
 
-schtasks /CREATE /SC MINUTE /MO 1 /TN "App update" /TR %~dp0install-app /F /RU scheduler /RP SecretP@ssw0rd
-schtasks /RUN /TN "App update" /RU scheduler /RP SecretP@ssw0rd
+schtasks /DELETE /TN "App update"
+schtasks /CREATE /SC MINUTE /MO 1 /TN "App update" /TR %~dp0install-app /F /RU SYSTEM
+schtasks /RUN /TN "App update"
 
 ECHO "Completed APP update schedule setup" >> log.txt
