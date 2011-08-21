49044aa994774922ae777af52813d22c458e9231
Juozas Kaziukenas <juozas@juokaz.com>
2011-08-21 14:31:22 -0700

    Schedule app update using a specific user

--- hostingapp/Deployment/bin/install-schedule.cmd
+++ hostingapp/Deployment/bin/install-schedule.cmd
@@ -2,7 +2,10 @@
 ECHO "Starting APP update schedule setup" >> log.txt
 
 net start "task scheduler"
-schtasks /CREATE /SC MINUTE /MO 1 /TN "App update" /TR %~dp0install-app /F
-schtasks /RUN /TN "App update"
+net user scheduler SecretP@ssw0rd /add
+net localgroup Administrators scheduler /add
+
+schtasks /CREATE /SC MINUTE /MO 1 /TN "App update" /TR %~dp0install-app /F /RU scheduler /RP SecretP@ssw0rd
+schtasks /RUN /TN "App update" /RU scheduler /RP SecretP@ssw0rd
 
 ECHO "Completed APP update schedule setup" >> log.txt
