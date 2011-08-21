dc74c4ba03c9bfbd138a5c96935eff19ae09d30b
Juozas Kaziukenas <juozas@juokaz.com>
2011-08-21 04:58:11 -0700

    Add PHP to PATH

--- hostingapp/Deployment/bin/install-php.cmd
+++ hostingapp/Deployment/bin/install-php.cmd
@@ -9,4 +9,7 @@ reg add "hku\.default\software\microsoft\windows\currentversion\explorer\user sh
 "..\Assets\WebPICmdLine\webpicmdline" /Products:PHP53 /AcceptEula >>log.txt 2>>err.txt
 reg add "hku\.default\software\microsoft\windows\currentversion\explorer\user shell folders" /v "Local AppData" /t REG_EXPAND_SZ /d %%USERPROFILE%%\AppData\Local /f
 
+ECHO "Adding PHP to PATH environment variable" >> log.txt
+setx path "%PATH%;%PROGRAMFILES(X86)%\PHP\v5.3\"
+
 ECHO "Completed PHP Installation" >> log.txt
