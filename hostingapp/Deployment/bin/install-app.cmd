aae1aedfb548d1d2d9cde19f4f3612416c23437c
Juozas Kaziukenas <juozas@juokaz.com>
2011-08-21 15:11:38 -0700

    Find PHP in program files, env PATH value seems to be not updated in tasks context

--- hostingapp/Deployment/bin/install-app.cmd
+++ hostingapp/Deployment/bin/install-app.cmd
@@ -4,11 +4,12 @@ cd /D %~dp0
 
 ECHO "Starting APP installation from storage account" >> log.txt
 
-php.exe storage.php update -c=apps -e=app.zip -n=sample.zip -t="..\..\sitesroot\0_new" >> log.txt 2>>err.txt
+%PROGRAMFILES(X86)%\PHP\v5.3\php storage.php update -c=apps -e=app.zip -n=sample.zip -t="..\..\sitesroot\0_new" >> log.txt 2>>err.txt
 
 if exist "..\..\sitesroot\0_new" (
-	rd /s /q "..\..\sitesroot\0\"
+	ren "..\..\sitesroot\0" "0_old"
 	ren "..\..\sitesroot\0_new" "0"
+	rd /s /q "..\..\sitesroot\0_old\"
 )
 
 ECHO "Completed APP Installation" >> log.txt
