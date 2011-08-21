515a62f4d58409ace797d150152d293f8652f7d9
Juozas Kaziukenas <juozas@juokaz.com>
2011-08-21 15:52:32 -0700

    Correct PHP path

--- hostingapp/Deployment/bin/install-app.cmd
+++ hostingapp/Deployment/bin/install-app.cmd
@@ -4,7 +4,7 @@ cd /D %~dp0
 
 ECHO "Starting APP installation from storage account" >> log.txt
 
-%PROGRAMFILES(X86)%\PHP\v5.3\php storage.php update -c=apps -e=app.zip -n=sample.zip -t="..\..\sitesroot\0_new" >> log.txt 2>>err.txt
+"%PROGRAMFILES(X86)%\PHP\v5.3\php" storage.php update -c=apps -e=app.zip -n=sample.zip -t="..\..\sitesroot\0_new" >> log.txt 2>>err.txt
 
 if exist "..\..\sitesroot\0_new" (
 	ren "..\..\sitesroot\0" "0_old"
