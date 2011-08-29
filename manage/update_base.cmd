91dadea0cf71275572dd657ee3aaf03ec38dc5ff
Juozas Kaziukenas <juozas@juokaz.com>
2011-08-29 08:48:51 -0700

    No useless output

--- manage/update_base.cmd
+++ manage/update_base.cmd
@@ -1,7 +1,9 @@
 @echo off
 
+REM Packaging app base
 cd ../hostingapp/
-cspack ServiceDefinition.csdef
+cspack ServiceDefinition.csdef > nul
 
+REM Uploading to azure blob storage
 cd ../manage/
-php deploy.php store-base -f=../hostingapp/ServiceDefinition.cspkg
+php deploy.php store-base -f=../hostingapp/ServiceDefinition.cspkg > nul
