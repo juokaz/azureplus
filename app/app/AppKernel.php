3daf79ae5a867b4475cff62359361cdf04faec58
Juozas Kaziukenas <juozas@juokaz.com>
2011-08-30 18:39:47 +0100

    Security managed with FOSUserBundle

--- app/app/AppKernel.php
+++ app/app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new FOS\RestBundle\FOSRestBundle(),
             new WebSpecies\Bundle\CloudBundle\CloudBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
--- app/src/WebSpecies/Bundle/CloudBundle/Entity/App.php
+++ app/src/WebSpecies/Bundle/CloudBundle/Entity/App.php
@@ -2,13 +2,35 @@
 
 namespace WebSpecies\Bundle\CloudBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="apps")
+ */
 class App
 {
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="string")
+     */
     private $name;
 
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
     private $url;
 
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
     private $storage_identifier;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="WebSpecies\Bundle\CloudBundle\Entity\User", inversedBy="apps")
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     */
+    protected $category;
     
     public function __construct($name)
     {
