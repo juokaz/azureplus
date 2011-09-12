8d0bbecccd249c7dae01b03f5e89860e52623ffc
Juozas Kaziukenas <juozas@juokaz.com>
2011-09-12 21:01:21 +0300

    Form support for new App configuration options

--- app/src/WebSpecies/Bundle/CloudBundle/Form/Type/AppType.php
+++ app/src/WebSpecies/Bundle/CloudBundle/Form/Type/AppType.php
@@ -4,12 +4,21 @@ namespace WebSpecies\Bundle\CloudBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilder;
+use WebSpecies\Bundle\CloudBundle\Entity\Configuration;
 
 class AppType extends AbstractType
 {
     public function buildForm(FormBuilder $builder, array $options)
     {
         $builder->add('name');
+        $builder->add('php_version', 'choice', array(
+            'choices'   => array(
+                Configuration::PHP_53 => 'PHP v5.3',
+                Configuration::PHP_52 => 'PHP v5.2'
+            )
+        ));
+        $builder->add('git_repository');
+        $builder->add('index_file');
     }
 
     public function getDefaultOptions(array $options)
