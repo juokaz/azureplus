9804adc2c53fa9cfcab7012de1f419baed2922b2
Juozas Kaziukenas <juozas@juokaz.com>
2011-09-21 13:34:13 +0100

    Auto install sql server php extensions

--- hostingapp/Deployment/bin/install-php.cmd
+++ hostingapp/Deployment/bin/install-php.cmd
@@ -6,7 +6,7 @@ cd "%~dp0appdata"
 cd..
 
 reg add "hku\.default\software\microsoft\windows\currentversion\explorer\user shell folders" /v "Local AppData" /t REG_EXPAND_SZ /d "%~dp0appdata" /f
-"..\Assets\WebPICmdLine\webpicmdline" /Products:PHP52,PHP53 /AcceptEula >>log.txt 2>>err.txt
+"..\Assets\WebPICmdLine\webpicmdline" /Products:PHP52,PHP53,SQLDriverPHP52IIS,SQLDriverPHP53IIS /AcceptEula >>log.txt 2>>err.txt
 reg add "hku\.default\software\microsoft\windows\currentversion\explorer\user shell folders" /v "Local AppData" /t REG_EXPAND_SZ /d %%USERPROFILE%%\AppData\Local /f
 
 ECHO "Adding PHP to PATH environment variable" >> log.txt
