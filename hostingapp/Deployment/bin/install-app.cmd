40722f61e3b04cd17516678942a0eb12cb8a461e
Juozas Kaziukenas <juozas@juokaz.com>
2011-09-22 13:01:48 +0100

    Include subfolders

--- hostingapp/Deployment/bin/install-app.cmd
+++ hostingapp/Deployment/bin/install-app.cmd
@@ -9,7 +9,7 @@ for /f "tokens=*" %%a in ('powershell -ExecutionPolicy Unrestricted .\config.ps1
 "%PROGRAMFILES(X86)%\PHP\v5.3\php" install.php "%APP_URL%" "app.zip" "..\..\sitesroot\0_new" >> log.txt 2>>err.txt
 
 if exist "..\..\sitesroot\0_new" (
-    ROBOCOPY "..\..\sitesroot\0_new" "..\..\sitesroot\0" /PURGE /NJS /NJH >> log.txt 2>>err.txt
+    ROBOCOPY "..\..\sitesroot\0_new" "..\..\sitesroot\0" /E /PURGE /NJS /NJH >> log.txt 2>>err.txt
 	rd /s /q "..\..\sitesroot\0_new\" >> log.txt 2>>err.txt
 )
 
