565708423e883e4df0744bcd639a7aa12d1d548b
Juozas Kaziukenas <juozas@juokaz.com>
2011-09-28 00:33:00 +0100

    Remove exiting FastCgi handlers before creating new ones

--- hostingapp/Deployment/bin/install-php.cmd
+++ hostingapp/Deployment/bin/install-php.cmd
@@ -1,6 +1,9 @@
 @echo off
 ECHO Starting PHP Installation >> log.txt
 
+REM Clear existing FastCGI mappers
+%windir%\system32\inetsrv\appcmd clear config /section:system.webServer/fastCGI 
+
 REM PHP 5.2 settings
 ECHO Setting PHP 5.2 Configuration >> log.txt
 %windir%\system32\inetsrv\appcmd set config /section:system.webServer/fastCGI /+"[fullPath='%ROLEROOT%\approot\php\v5.2\php-cgi.exe',maxInstances='4',instanceMaxRequests='10000',requestTimeout='180',activityTimeout='180']" >> log.txt 2>>err.txt
