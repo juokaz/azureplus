475dd87df5275bba646269abb9e91d7d9b1e0383
Juozas Kaziukenas <juozas@juokaz.com>
2011-09-27 23:04:13 +0100

    Quote php folders, requirement by the comand

--- manage/update_base.cmd
+++ manage/update_base.cmd
@@ -2,10 +2,10 @@
 
 REM php setup
 echo Downloading PHP 5.2
-rd /s /q ../hostingapp/Deployment/php/v5.2
+rd /s /q "../hostingapp/Deployment/php/v5.2"
 php php.php http://windows.php.net/downloads/releases/php-5.3.8-nts-Win32-VC9-x86.zip ../hostingapp/Deployment/php/v5.2
 echo Downloading PHP 5.3
-rd /s /q ../hostingapp/Deployment/php/v5.3
+rd /s /q "../hostingapp/Deployment/php/v5.3"
 php php.php http://windows.php.net/downloads/releases/php-5.2.17-nts-Win32-VC6-x86.zip ../hostingapp/Deployment/php/v5.3
 
 REM Packaging app base
