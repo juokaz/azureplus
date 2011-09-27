7a707b40b90faf8b47e8f91a1f91f3af0db8d42c
Juozas Kaziukenas <juozas@juokaz.com>
2011-09-27 22:44:49 +0100

    Use PHP from approot folder and monitor php.ini file

--- hostingapp/Deployment/bin/install-php.cmd
+++ hostingapp/Deployment/bin/install-php.cmd
@@ -1,15 +1,25 @@
 @echo off
 ECHO "Starting PHP Installation" >> log.txt
 
-md "%~dp0appdata"
-cd "%~dp0appdata"
-cd..
+REM PHP 5.2 settings
+%windir%\system32\inetsrv\appcmd set config /section:system.webServer/fastCGI /+"[fullPath='%ROLEROOT%\approot\php\v5.2\php-cgi.exe',maxInstances='4',instanceMaxRequests='10000',requestTimeout='180',activityTimeout='180']"
 
-reg add "hku\.default\software\microsoft\windows\currentversion\explorer\user shell folders" /v "Local AppData" /t REG_EXPAND_SZ /d "%~dp0appdata" /f
-"..\Assets\WebPICmdLine\webpicmdline" /Products:PHP52,PHP53,SQLDriverPHP52IIS,SQLDriverPHP53IIS /AcceptEula >>log.txt 2>>err.txt
-reg add "hku\.default\software\microsoft\windows\currentversion\explorer\user shell folders" /v "Local AppData" /t REG_EXPAND_SZ /d %%USERPROFILE%%\AppData\Local /f
+%windir%\system32\inetsrv\appcmd.exe set config -section:system.webServer/fastCgi /+"[fullPath='%ROLEROOT%\approot\php\v5.2\php-cgi.exe'].environmentVariables.[name='PHP_FCGI_MAX_REQUESTS',value='10000']"
+
+%windir%\system32\inetsrv\appcmd.exe set config -section:system.webServer/fastCgi /+"[fullPath='%ROLEROOT%\approot\php\v5.2\php-cgi.exe'].environmentVariables.[name='PHPRC',value='%ROLEROOT%\sitesroot\0']"
+
+%windir%\system32\inetsrv\appcmd.exe set config -section:system.webServer/fastCgi /"[fullPath='%ROLEROOT%\approot\php\v5.2\php-cgi.exe']".monitorChangesTo:"%ROLEROOT%\sitesroot\0\php.ini" /commit:apphost 
+
+REM PHP 5.3 settings
+%windir%\system32\inetsrv\appcmd set config /section:system.webServer/fastCGI /+"[fullPath='%ROLEROOT%\approot\php\v5.3\php-cgi.exe',maxInstances='4',instanceMaxRequests='10000',requestTimeout='180',activityTimeout='180']"
+
+%windir%\system32\inetsrv\appcmd.exe set config -section:system.webServer/fastCgi /+"[fullPath='%ROLEROOT%\approot\php\v5.3\php-cgi.exe'].environmentVariables.[name='PHP_FCGI_MAX_REQUESTS',value='10000']"
+
+%windir%\system32\inetsrv\appcmd.exe set config -section:system.webServer/fastCgi /+"[fullPath='%ROLEROOT%\approot\php\v5.3\php-cgi.exe'].environmentVariables.[name='PHPRC',value='%ROLEROOT%\sitesroot\0']"
+
+%windir%\system32\inetsrv\appcmd.exe set config -section:system.webServer/fastCgi /"[fullPath='%ROLEROOT%\approot\php\v5.3\php-cgi.exe']".monitorChangesTo:"%ROLEROOT%\sitesroot\0\php.ini" /commit:apphost 
 
 ECHO "Adding PHP to PATH environment variable" >> log.txt
-powershell -command [Environment]::SetEnvironmentVariable('Path', [Environment]::GetEnvironmentVariable('Path', 'Machine') + ';%PROGRAMFILES(X86)%\PHP\v5.3\', 'Machine')
+powershell -command [Environment]::SetEnvironmentVariable('Path', [Environment]::GetEnvironmentVariable('Path', 'Machine') + ';%ROLEROOT%\approot\php\v5.3\', 'Machine')
 
 ECHO "Completed PHP Installation" >> log.txt
