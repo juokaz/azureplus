e013b1573fc2647b3efd1c3b7323b3655daf8589
Juozas Kaziukenas <juozas@juokaz.com>
2011-09-27 23:24:06 +0100

    Log appcmd outputs

--- hostingapp/Deployment/bin/install-php.cmd
+++ hostingapp/Deployment/bin/install-php.cmd
@@ -3,22 +3,22 @@ ECHO Starting PHP Installation >> log.txt
 
 REM PHP 5.2 settings
 ECHO Setting PHP 5.2 Configuration >> log.txt
-%windir%\system32\inetsrv\appcmd set config /section:system.webServer/fastCGI /+"[fullPath='%ROLEROOT%\approot\php\v5.2\php-cgi.exe',maxInstances='4',instanceMaxRequests='10000',requestTimeout='180',activityTimeout='180']"
+%windir%\system32\inetsrv\appcmd set config /section:system.webServer/fastCGI /+"[fullPath='%ROLEROOT%\approot\php\v5.2\php-cgi.exe',maxInstances='4',instanceMaxRequests='10000',requestTimeout='180',activityTimeout='180']" >> log.txt 2>>err.txt
 
-%windir%\system32\inetsrv\appcmd.exe set config -section:system.webServer/fastCgi /+"[fullPath='%ROLEROOT%\approot\php\v5.2\php-cgi.exe'].environmentVariables.[name='PHP_FCGI_MAX_REQUESTS',value='10000']"
+%windir%\system32\inetsrv\appcmd.exe set config -section:system.webServer/fastCgi /+"[fullPath='%ROLEROOT%\approot\php\v5.2\php-cgi.exe'].environmentVariables.[name='PHP_FCGI_MAX_REQUESTS',value='10000']" >> log.txt 2>>err.txt
 
-%windir%\system32\inetsrv\appcmd.exe set config -section:system.webServer/fastCgi /+"[fullPath='%ROLEROOT%\approot\php\v5.2\php-cgi.exe'].environmentVariables.[name='PHPRC',value='%ROLEROOT%\sitesroot\0']"
+%windir%\system32\inetsrv\appcmd.exe set config -section:system.webServer/fastCgi /+"[fullPath='%ROLEROOT%\approot\php\v5.2\php-cgi.exe'].environmentVariables.[name='PHPRC',value='%ROLEROOT%\sitesroot\0']" >> log.txt 2>>err.txt
 
-%windir%\system32\inetsrv\appcmd.exe set config -section:system.webServer/fastCgi /"[fullPath='%ROLEROOT%\approot\php\v5.2\php-cgi.exe']".monitorChangesTo:"%ROLEROOT%\sitesroot\0\php.ini" /commit:apphost 
+%windir%\system32\inetsrv\appcmd.exe set config -section:system.webServer/fastCgi /"[fullPath='%ROLEROOT%\approot\php\v5.2\php-cgi.exe']".monitorChangesTo:"%ROLEROOT%\sitesroot\0\php.ini" /commit:apphost  >> log.txt 2>>err.txt
 
 REM PHP 5.3 settings
 ECHO Setting PHP 5.3 Configuration >> log.txt
-%windir%\system32\inetsrv\appcmd set config /section:system.webServer/fastCGI /+"[fullPath='%ROLEROOT%\approot\php\v5.3\php-cgi.exe',maxInstances='4',instanceMaxRequests='10000',requestTimeout='180',activityTimeout='180']"
+%windir%\system32\inetsrv\appcmd set config /section:system.webServer/fastCGI /+"[fullPath='%ROLEROOT%\approot\php\v5.3\php-cgi.exe',maxInstances='4',instanceMaxRequests='10000',requestTimeout='180',activityTimeout='180']" >> log.txt 2>>err.txt
 
-%windir%\system32\inetsrv\appcmd.exe set config -section:system.webServer/fastCgi /+"[fullPath='%ROLEROOT%\approot\php\v5.3\php-cgi.exe'].environmentVariables.[name='PHP_FCGI_MAX_REQUESTS',value='10000']"
+%windir%\system32\inetsrv\appcmd.exe set config -section:system.webServer/fastCgi /+"[fullPath='%ROLEROOT%\approot\php\v5.3\php-cgi.exe'].environmentVariables.[name='PHP_FCGI_MAX_REQUESTS',value='10000']" >> log.txt 2>>err.txt
 
-%windir%\system32\inetsrv\appcmd.exe set config -section:system.webServer/fastCgi /+"[fullPath='%ROLEROOT%\approot\php\v5.3\php-cgi.exe'].environmentVariables.[name='PHPRC',value='%ROLEROOT%\sitesroot\0']"
+%windir%\system32\inetsrv\appcmd.exe set config -section:system.webServer/fastCgi /+"[fullPath='%ROLEROOT%\approot\php\v5.3\php-cgi.exe'].environmentVariables.[name='PHPRC',value='%ROLEROOT%\sitesroot\0']" >> log.txt 2>>err.txt
 
-%windir%\system32\inetsrv\appcmd.exe set config -section:system.webServer/fastCgi /"[fullPath='%ROLEROOT%\approot\php\v5.3\php-cgi.exe']".monitorChangesTo:"%ROLEROOT%\sitesroot\0\php.ini" /commit:apphost 
+%windir%\system32\inetsrv\appcmd.exe set config -section:system.webServer/fastCgi /"[fullPath='%ROLEROOT%\approot\php\v5.3\php-cgi.exe']".monitorChangesTo:"%ROLEROOT%\sitesroot\0\php.ini" /commit:apphost  >> log.txt 2>>err.txt
 
 ECHO Completed PHP Installation >> log.txt
