79dfc3590de044db84961f452e5e6f2a20f36507
Juozas Kaziukenas <juozas@juokaz.com>
2011-09-28 09:11:23 +0100

    Force-run the app update after creation of schedule

--- hostingapp/Deployment/bin/install-schedule.cmd
+++ hostingapp/Deployment/bin/install-schedule.cmd
@@ -1,9 +1,13 @@
 @echo off
-ECHO "Starting APP update schedule setup" >> log.txt
+ECHO Starting APP update schedule setup >> log.txt
 
+REM start the scheduler
 net start "task scheduler"
-
+REM delete task if it exists
 schtasks /DELETE /TN "App update" /F
+REM create app update task, runs every minute
 schtasks /CREATE /SC MINUTE /MO 1 /TN "App update" /TR %~dp0install-app /F /RU SYSTEM
+REM force run it
+schtasks /RUN /TN "App update"
 
-ECHO "Completed APP update schedule setup" >> log.txt
+ECHO Completed APP update schedule setup >> log.txt
