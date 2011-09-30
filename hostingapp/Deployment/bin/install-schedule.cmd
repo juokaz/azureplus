08ba2baf620db4e0859c30eccbbf3e661c273c07
Juozas Kaziukenas <juozas@juokaz.com>
2011-09-30 16:37:29 +0100

    Update service

--- hostingapp/Deployment/bin/install-schedule.cmd
+++ hostingapp/Deployment/bin/install-schedule.cmd
@@ -2,7 +2,7 @@
 
 ECHO Starting APP update setup >> log.txt
 
-..\Assets\installutil /u ..\Assets\AzureDownloader.exe >> log.txt
-..\Assets\installutil /i ..\Assets\AzureDownloader.exe >> log.txt
+..\Assets\installutil /u /LogToConsole=false ..\Assets\AzureDownloader.exe >> log.txt 2>>err.txt
+..\Assets\installutil /i /LogToConsole=false ..\Assets\AzureDownloader.exe >> log.txt 2>>err.txt
 
 ECHO Completed APP update setup >> log.txt
