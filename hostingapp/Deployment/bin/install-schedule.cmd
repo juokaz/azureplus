ae995c43a7d258138e9f250791bfcf585ca2b787
Juozas Kaziukenas <juozas@juokaz.com>
2011-10-01 00:05:46 +0100

    Compile AzureDownloader for Any CPU and make use of InstallUtil available in the system

--- hostingapp/Deployment/bin/install-schedule.cmd
+++ hostingapp/Deployment/bin/install-schedule.cmd
@@ -2,8 +2,13 @@
 
 ECHO Starting APP update setup >> log.txt
 
-..\Assets\installutil /u /LogToConsole=false ..\Assets\AzureDownloader.exe >> log.txt 2>>err.txt
-..\Assets\installutil /i /LogToConsole=false ..\Assets\AzureDownloader.exe >> log.txt 2>>err.txt
+IF EXIST %WINDIR%\Microsoft.NET\Framework64 (
+%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\installutil.exe /u /LogToConsole=false ..\Assets\AzureDownloader.exe >> log.txt 2>>err.txt
+%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\installutil.exe /i /LogToConsole=false ..\Assets\AzureDownloader.exe >> log.txt 2>>err.txt
+) ELSE (
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\installutil.exe /u /LogToConsole=false ..\Assets\AzureDownloader.exe >> log.txt 2>>err.txt
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\installutil.exe /i /LogToConsole=false ..\Assets\AzureDownloader.exe >> log.txt 2>>err.txt
+)
 
 ECHO Completed APP update setup >> log.txt
 
