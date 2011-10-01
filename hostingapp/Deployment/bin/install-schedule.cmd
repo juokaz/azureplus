9f48db69cb7d881a0783f97341ec0eaaae7b06f3
Juozas Kaziukenas <juozas@juokaz.com>
2011-10-01 01:25:23 +0100

    Log service start

--- hostingapp/Deployment/bin/install-schedule.cmd
+++ hostingapp/Deployment/bin/install-schedule.cmd
@@ -10,7 +10,7 @@ IF EXIST %WINDIR%\Microsoft.NET\Framework64 (
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\installutil.exe /i /LogToConsole=false ..\Assets\AzureDownloader.exe >> log.txt 2>>err.txt
 )
 
-net start "Azure Downloader"
+net start "Azure Downloader" >> log.txt 2>>err.txt
 
 ECHO Completed APP update setup >> log.txt
 
